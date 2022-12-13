5d82c3a5e97d786fcf0817dcd5bf0a3d3bed235c
sameyvazi <saman3yvazi@gmail.com>
2022-12-13 12:12:40 +0330

    ticket search

--- ticket/views.py
+++ ticket/views.py
@@ -2,6 +2,8 @@ from rest_framework import viewsets, permissions
 from ticket.serializers import TicketSerializer, TicketReadSerializer
 from .models import Ticket
 from django.db.models import Q
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 
 class TicketViewSet(viewsets.ModelViewSet):
@@ -17,3 +19,10 @@ class TicketViewSet(viewsets.ModelViewSet):
 
     search_fields = ('id', 'user__id', 'department__title')
     ordering_fields = ('create_time', 'modified_time')
+
+    @action(detail=False)
+    def ticket_search(self, request):
+        title = request.GET.get('q')
+        tickets = Ticket.objects.filter(subject__icontains=title)
+        context = " - ".join([f"{ticket.id}" for ticket in tickets])
+        return Response(context)
